e3fbb73d93ac0b4c670fe966fae96b4caf746894
Christian Reichel <chrstn.rchl@gmail.com>
2020-09-14 12:47:55 +0200

    Refactor namings of modules

---
+++ estimator-mock/src/main.cpp
@@ -0,0 +1,62 @@
+#include "WebsocketServer.h"
+#include "domainvalue/DelayPerData.h"
+
+#include <iostream>
+#include <thread>
+#include <asio/io_service.hpp>
+#include <utility>
+#include <fstream>
+#include <chrono>
+
+//The port number the WebSocket server listens on
+#define PORT_NUMBER 8080
+
+std::vector<DelayPerData> readFromFile(const std::string& fileNameWithPath) {
+    std::fstream file (fileNameWithPath, std::ios::in);
+
+    if (!file.is_open()) {
+        std::cerr << "Cannot read file. Check if it exist or record a session first.";
+    }
+
+    std::vector<DelayPerData> data;
+
+    std::string line;
+    while (std::getline(file, line)) {
+        DelayPerData singleData(line);
+        data.push_back(singleData);
+    }
+    file.close();
+    return data;
+}
+
+int main(int argc, char* argv[])
+{
+	//Create the event loop for the main thread, and the WebSocket server
+	asio::io_service mainEventLoop;
+	WebsocketServer server;
+	
+	//Start the networking thread
+	std::thread serverThread([&server]() {
+		server.run(PORT_NUMBER);
+	});
+
+    std::vector<DelayPerData> data = readFromFile("./test.mock");
+
+    //Start the event loop for the main thread
+    asio::io_service::work work(mainEventLoop);
+
+    while(true) {
+        // Skip sending the messages
+        if (server.numConnections() == 0) continue;
+
+        for (const auto& singleData : data) {
+            std::cout << "Sending data after " + std::to_string(singleData.delay) + "ms\n";
+            std::this_thread::sleep_for(std::chrono::milliseconds((int) singleData.delay));
+            server.broadcastMessage(singleData.data);
+        }
+    }
+
+    mainEventLoop.run();
+	
+	return 0;
+}
