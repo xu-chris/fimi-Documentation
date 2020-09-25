1dab281a9a0f3e8520f5ffd93be0ae5acce66d85
Chris Reichel <chrisreichel@pm.me>
2020-09-25 02:15:57 +0200

    Minor cleanups

--- application/Assets/_Project/Scripts/DomainObjects/Rules/SymmetryRule.cs
+++ application/Assets/_Project/Scripts/DomainObjects/Rules/SymmetryRule.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using _Project.Scripts.Core;
 using UnityEngine;
-using Object = UnityEngine.Object;
-using Vector3 = UnityEngine.Vector3;
 
 namespace _Project.Scripts.DomainObjects.Rules
 {
@@ -17,22 +15,20 @@ namespace _Project.Scripts.DomainObjects.Rules
         public bool IsInvalidated(List<Bone> leftBoneObjects, List<Bone> rightBoneObjects, Bone referenceBone)
         {
             var reflectedLeftBoneVectors = new List<Vector3>();
-                
+
             // Reflect bone vectors of one list
             foreach (var leftBoneObject in leftBoneObjects)
             {
-                var flippedVector = RotateAround(leftBoneObject.boneVector, referenceBone.boneVector, referenceBone.boneVector, 180);
+                var flippedVector = RotateAround(leftBoneObject.boneVector, referenceBone.boneVector,
+                    referenceBone.boneVector, 180);
                 reflectedLeftBoneVectors.Add(flippedVector);
-
             }
 
             var runningDistance = 0f;
-            
+
             foreach (var leftBoneVector in reflectedLeftBoneVectors)
             foreach (var rightBoneObject in rightBoneObjects)
-            {
                 runningDistance += Vector3.Distance(leftBoneVector, rightBoneObject.boneVector);
-            }
 
             return runningDistance > tolerance;
         }
@@ -48,10 +44,10 @@ namespace _Project.Scripts.DomainObjects.Rules
                    string.Join(", ", leftBones.ToArray()) + ", right bones: " +
                    string.Join(", ", leftBones.ToArray()) + ", center / reference bone: " + centerBone;
         }
-        
+
         private Vector3 RotateAround(Vector3 fromPoint, Vector3 aroundPoint, Vector3 aroundAxis, float angle)
         {
-            Vector3 vector3 = Quaternion.AngleAxis(angle, aroundAxis) * (fromPoint - aroundPoint);
+            var vector3 = Quaternion.AngleAxis(angle, aroundAxis) * (fromPoint - aroundPoint);
             return aroundPoint + vector3;
         }
     }
